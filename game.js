6c6a261909ac7d5cd7114a2e61673e274bd847aa
WilliamLuth <williamluth@gmail.com>
2021-05-12 21:33:34 +0800

    added

--- game.js
+++ game.js
@@ -183,104 +183,97 @@ const textNodes = [{
     },
     {
         id: 3,
-        text: 'After leaving the merchant you start to feel tired and stumble upon a small town next to a dangerous looking castle.',
+        text: 'Well... i assume you already read the previous topics right? or.. no :( ? well here is for this part :).',
         options: [{
-                text: 'Explore the castle',
+                text: 'My current thoughts',
                 nextText: 4
             },
             {
-                text: 'Find a room to sleep at in the town',
-                nextText: 5
+                text: 'Why I made this?',
+                nextText: 9
             },
             {
-                text: 'Find some hay in a stable to sleep in',
-                nextText: 6
+                text: 'The last',
+                nextText: 10
             }
         ]
     },
     {
         id: 4,
-        text: 'You are so tired that you fall asleep while exploring the castle and are killed by some terrible monster in your sleep.',
+        text: 'Well from what happening right now, it makes me think that everything is done tbh, i mean like hh idk if you still have feelings for me or not but i cant just stay optimist but for me it still remains the same :"). you know because we dont even talk anymore, well i know it is my fault but god i wish it went different way. also yes i sound very selfish',
         options: [{
-            text: 'Restart',
-            nextText: -1
+            text: 'next',
+            nextText: 5
         }]
     },
     {
         id: 5,
-        text: 'Without any money to buy a room you break into the nearest inn and fall asleep. After a few hours of sleep the owner of the inn finds you and has the town guard lock you in a cell.',
+        text: 'Not gonna lie.. i sometimes imagining us still together and do lots of fun and dreamed about it as well. iknow it sounds naive but it is what it is. i never regret having you in my life because you made me feel like i dont need anybody else for everything.',
         options: [{
-            text: 'Restart',
-            nextText: -1
+            text: 'next',
+            nextText: 6
         }]
     },
     {
         id: 6,
-        text: 'You wake up well rested and full of energy ready to explore the nearby castle.',
+        text: 'and im so sorry for everything i have done to you, everything that hurts you so bad (well i know almost everything that i did), I know that in a relationship it has to be take and give but all i did is just taking while you are giving everything hh i feel so shamed about it. im sorry dude',
         options: [{
-            text: 'Explore the castle',
+            text: 'next',
             nextText: 7
         }]
     },
     {
         id: 7,
-        text: 'While exploring the castle you come across a horrible monster in your path.',
+        text: 'and uh i really miss your smile :"). also i really miss your chat dude, i wanna talk to you again if that possible. i know by making this means i broke my promise not to contact you again after 3 days missing but what can i do? i mean when you blocked me at WA i already texted you at line but nothing, also i stil couldnt text you in instagram either due to full memory',
         options: [{
-                text: 'Try to run',
-                nextText: 8
-            },
-            {
-                text: 'Attack it with your sword',
-                requiredState: (currentState) => currentState.sword,
-                nextText: 9
-            },
-            {
-                text: 'Hide behind your shield',
-                requiredState: (currentState) => currentState.shield,
-                nextText: 10
-            },
-            {
-                text: 'Throw the blue goo at it',
-                requiredState: (currentState) => currentState.blueGoo,
-                nextText: 11
-            }
-        ]
+            text: 'next',
+            nextText: 8
+
+        }]
     },
     {
         id: 8,
-        text: 'Your attempts to run are in vain and the monster easily catches.',
+        text: 'Last, i hope youre going to do just fine at malaysia:). i know youre going to make lots of friend and maybe just maybe find another S.O idk, my point is ill support you for everything you want and i just want you to be happy that is all, if youre happy so as I',
         options: [{
-            text: 'Restart',
-            nextText: -1
+            text: 'next topic',
+            nextText: 3
         }]
     },
     {
         id: 9,
-        text: 'You foolishly thought this monster could be slain with a single sword.',
+        text: 'why you asked? well first this could be the last time ill see you because no one knows what will happen next right? also because idk i still feel lots of feeling towards you. Second, damn dude you really hit my heart last time when you gave me that bday present and i just dont want you leave without having my gift for you. so because this could be the last one which i hope not, i want this to be really special because im giving it to a special someone :)',
         options: [{
-            text: 'Restart',
-            nextText: -1
+            text: 'next topic?',
+            nextText: 3
         }]
     },
     {
         id: 10,
-        text: 'The monster laughed as you hid behind your shield and ate you.',
+        text: 'Hey hey, you reached the last section:D. Im sad that this is the last part and knowing that you are going away but i dont want to waste any more time again because i dont know when youre going to malaysia, so i dont want to be late for that hehe',
         options: [{
-            text: 'Restart',
-            nextText: -1
+            text: 'next',
+            nextText: 11
         }]
     },
     {
         id: 11,
-        text: 'You threw your jar of goo at the monster and it exploded. After the dust settled you saw the monster was destroyed. Seeing your victory you decide to claim this castle as your and live out the rest of your days there.',
+        text: 'and uh here i wanna confess and please dont be mad at me dude, i know this means violating your privacy and makes you uncomvy but damn dude i cant delete all of your pics i mean i took them when i think its my best time with you and uh they are one thing that keeps you in my head and i look at them every single time i miss you.',
         options: [{
-            text: 'Congratulations. Play Again.',
+            text: 'next',
+            nextText: 29
+        }]
+    },
+    {
+        id: 29,
+        text: 'owh one more thing the necklace inside the box it is actually coupled necklace i have the other part hehe, but dont get me wrong i gave you that because you are still my special someone and i want you to have that because idk you love cat and uh i want you to remember me honestly hhh, but if you dont want it you can just throw it away or something idk but if you do it just dont tell me anything about it heheh ',
+        options: [{
+            text: 'next',
             nextText: 12
         }]
     },
     {
         id: 12,
-        text: 'I love you the most <33333333, yup you dont have to count there are 8 of them :D, also this is the end so I hope youll be fine there ,take care:D',
+        text: ' hey this is the last last and i just want to say that I love you the most dude <33333333, yup you dont have to count there are 8 of them :D, also this is the end so I hope youll be fine there ,take care:D and i hope that you will have a nice day :D. I will be waiting for your chats whenever you want ',
         options: [{
             text: 'End',
             nextText: -1
